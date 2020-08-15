8f43c9800d19375b113831086b98e09c727ca092
danyasatsuk <dsatsukapp@gmail.com>
2020-08-16 01:25:30 +0700

    Init

---
+++ precache-manifest.b2ebcba5ef3119817423a340234b4aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539799ac60874126b604ea91c02e1028",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72551d0ffa8074e043a8",
+    "url": "/static/css/main.b2e78aac.chunk.css"
+  },
+  {
+    "revision": "751d4c6471c945c52465",
+    "url": "/static/js/2.043d51b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.043d51b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72551d0ffa8074e043a8",
+    "url": "/static/js/main.8c92dcf2.chunk.js"
+  },
+  {
+    "revision": "8755375e5f3f8c82f4f5",
+    "url": "/static/js/runtime-main.2daa0d0d.js"
+  }
+]);
